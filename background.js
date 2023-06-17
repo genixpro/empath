4446074fe8867801f68519209336b70152807fe7
Bradley Arsenault <bradley@checksum.ai>
2023-06-17 14:02:46 -0700

    Initial commit of empath code

---
+++ background.js
@@ -0,0 +1,133 @@
+const openAIAPIKey = '';
+// const promptEnding = "Try to preserve the main point of what is being said. Rewrite the following:\n";
+// const prompts = [
+//   {
+//     "id": "nice",
+//     "title": "Nice and Friendly",
+//     "prompt": `Rewrite the following text to be nice and friendly. ` +
+//       `Remove any inappropriate content and try to make it professional and respectful. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "bold",
+//     "title": "Bold and Ambitious",
+//     "prompt": `Rewrite the following text to be bolder and more ambitious. ` +
+//       `Make it sound grand and amazing. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "happy",
+//     "title": "Happy and Optimistic",
+//     "prompt": `Rewrite the following text to be happier and more optimistic. ` +
+//       `It should put me in a positive mood. ` +
+//       `Use positive words and a cheerful tone. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "logical",
+//     "title": "Terse and Logical",
+//     "prompt": `Rewrite the following text to be terse and logical. ` +
+//       `It should be straight to the point. ` +
+//       `Get rid of the fluff. ` +
+//       `Remove any and all unnecessary words. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "funny",
+//     "title": "Funny and Humorous",
+//     "prompt": `Rewrite the following text to be humorous and funny. ` +
+//       `Add in a joke here or there. ` +
+//       `Make it more light hearted and fun. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "official",
+//     "title": "Official and Bureaucratic",
+//     "prompt": `Rewrite the following text to sound more official and bureaucratic. ` +
+//       `It should sound like a robot wrote it, or a very astute communications person. ` +
+//       `Make it sound like how a government, large corporation or lawyer would write a press release. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "romantic",
+//     "title": "Romantic and Loving",
+//     "prompt": `Rewrite the following text to be romantic and loving. ` +
+//       `Make it sound like a cheesy line from a romantic comedy movie. ` +
+//       promptEnding,
+//   },
+//   {
+//     "id": "flirty",
+//     "title": "Flirty and Sexy",
+//     "prompt": `Rewrite the following text to be flirty and sexy. ` +
+//       `It should sound suave and charming. ` +
+//       `Maybe even a little bit naughty, but not over-the-top. ` +
+//       `Flirtatious but not offensive. ` +
+//       promptEnding,
+//   },
+// ]
+
+function findPromptById(id) {
+  for (let prompt of prompts) {
+    if (prompt.id === id) {
+      return prompt;
+    }
+  }
+  return null;
+}
+
+function replaceSelectedText(newText, tab) {
+  // Send a message to the content script to replace the selected text
+  browser.tabs.sendMessage(tab.id, {
+    action: "replaceText",
+    newText: newText,
+  });
+}
+
+
+function handleClick(info, tab) {
+  const prompt = findPromptById(info.menuItemId);
+  const selectedText = info.selectionText;
+  const chatGptCommand = `${prompt.prompt}\n${selectedText}`;
+
+  // Make an API call using the fetch API
+  fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST", // Specify the HTTP method (e.g., POST, GET, etc.)
+    headers: {
+      "Content-Type": "application/json", // Set the Content-Type header if required
+      "Authorization": `Bearer ${openAIAPIKey}`,
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": chatGptCommand}],
+      "temperature": 0.7
+    }), // Pass any data in the request body
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const text = data.choices[0].message.content;
+      replaceSelectedText(text, tab);
+    })
+    .catch((error) => {
+      replaceSelectedText(JSON.stringify(error, null, 4), tab);
+    });
+}
+
+//
+// browser.contextMenus.create({
+//   id: "rewrite-text-base",
+//   title: "Rewrite text",
+//   contexts: ["selection"],
+// });
+//
+// for (let promptInfo of prompts) {
+//   browser.contextMenus.create({
+//     id: promptInfo.id,
+//     parentId: "rewrite-text-base",
+//     title: promptInfo.title,
+//     contexts: ["selection"],
+//   });
+// }
+//
+// browser.contextMenus.onClicked.addListener(handleClick);
+//
+//
